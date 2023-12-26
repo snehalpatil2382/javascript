f8ac3fa31be456d5117cd5f382aa98b8c5481f52
snehalpatil2382 <snehalpatil2382@gmail.com>
2023-12-26 09:45:02 +0530

    variable assignment
    variable assignment

---
+++ helloWord.js
@@ -0,0 +1,21 @@
+console.log("Hello JS ,I am Snehal");
+var num;
+console.log(num);
+var num=10;
+console.log(num);
+var myName="Snehal Patil"; // Varibale Declartion
+console.log(myName); // undefined
+console.log("Full name :",myName);
+
+
+var myName; // Varibale Declartion
+console.log(myName); // undefined
+
+myName = "Snehal"; // Variable Intialization
+console.log("First Name: ",myName);
+
+var age = "21"; // Varibale declaration and Initialization
+console.log("Age:", age);
+
+myName = "Snehal Patil"; // Varibale update
+console.log("Full name: ", myName);
