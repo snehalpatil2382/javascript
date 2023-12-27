1518171bb1c026c40559a9c7266834c2b3c34ece
snehalpatil2382 <snehalpatil2382@gmail.com>
2023-12-27 15:21:28 +0530

    adding datatype and swapping file
    adding datatype and swapping file

---
+++ 02variableBasicsAssign.js
@@ -0,0 +1,32 @@
+console.log("--------------------> 1 <---------------------");
+console.log("-------------------- Number ------------------------------");
+var num1=27;
+var num2=10;
+var num3=12;
+console.log("Number:",num1,',',"DataType:",typeof num1);
+console.log("Number:",num2,',',"DataType:",typeof num2);
+console.log("Number:",num3,',',"DataType:",typeof num3);
+
+console.log("-------------------- String ------------------------------");
+var name1="Snehal";
+var name2="Sakshi";
+var name3="Rohini";
+console.log("Name:",name1,',',"DataType:",typeof name1);
+console.log("Name:",name2,',',"DataType:",typeof name2);
+console.log("Name:",name3,',',"DataType:",typeof name3);
+
+console.log("-------------------- Boolean ------------------------------");
+var result1 = true;
+var result2= false;
+var result3 = true;
+console.log("ResultStaus :",result1,',',"DataType:",typeof result1);
+console.log("ResultStaus :",result2,',',"DataType:",typeof result2);
+console.log("ResultStaus :",result3,',',"DataType:",typeof result3);
+console.log("-------------------- 2 ----------------------");
+console.log("--------------- Difference Between Null and Undefined ----------------------");
+console.log("null it can be assigned to a variable as a representation of no value.");
+console.log("undefined means a variable has been declared but has yet not been assigned a value.");
+
+
+
+
