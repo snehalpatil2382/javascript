5d11ce3ba3a9552e49a552690eeeabb97c6880bc
snehalpatil2382 <snehalpatil2382@gmail.com>
2024-01-24 17:42:26 +0530

    dom folder and files added
    dom folder and files added

---
+++ dom/14_main.js
@@ -0,0 +1,11 @@
+const myProfile = document.querySelector("h1");
+myProfile.style.color="orange";
+
+const techStack = document.querySelector("h2");
+techStack.style.color="red";
+const myHobbies = document.querySelector("h3");
+techStack.style.fontWeight="bold";
+
+const project=document.querySelector("#project");
+project.style.color="green";
+project.style.fontWeight="bold";
